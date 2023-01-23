6ffe2e7de9e24a14f74e05ee8c691df4f4e61243
John Henderson <john.henderson@energyweb.org>
2023-01-23 20:10:20 +0000

    add some logs

--- speedtest.py
+++ speedtest.py
@@ -3,19 +3,22 @@ import subprocess
 import time
 
 # Set the number of seconds between each speedtest
-interval = 20
+interval = 5
 
 # Set the number of tests to run
-num_tests = 5 
+num_tests = 1 
+
+start_timestamp = time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())
 
 # Open a CSV file for writing
-with open('speedtest_results.csv', mode='w') as csv_file:
+with open(f'{start_timestamp}_speedtest_results.csv', mode='w') as csv_file:
     fieldnames = ['Timestamp', 'Ping (ms)', 'Download (Mbps)', 'Upload (Mbps)']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
 
     # Perform the tests
     for i in range(num_tests):
+        print(f'speedtest {i}')
         # Get the current time
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 
